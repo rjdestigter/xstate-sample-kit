4d8d21b92e30e9ecd1bcde9fa98f5435240a1716
rjdestigter <johndestigter@gmail.com>
2020-02-22 18:20:02 -0700

    Working on that routing module.

--- src/modules/router/index.tsx
+++ src/modules/router/index.tsx
@@ -69,7 +69,7 @@ const UserDetail = (props: { id: number }) => {
     return () => {
       didCancel = true;
     };
-  });
+  }, [props.id]);
 
   if (user === null) {
     return <Redirect to={"notFound"} />;
@@ -143,7 +143,7 @@ const routes = [
   /users/,
 ]
 
-const gotoEventFromUrl = (rawUrl: string): Event => {
+const makeGotoEventFromUrl = (rawUrl: string): Event => {
   const url = rawUrl.replace(/^\/|\/$/, '')
 
   const { event } = routes.reduce(
@@ -188,7 +188,7 @@ export default () => {
 
   React.useEffect(
     () => {
-      const event = gotoEventFromUrl(history.location.pathname)
+      const event = makeGotoEventFromUrl(history.location.pathname)
       send(event)
       setReady(true)
     },
@@ -201,7 +201,7 @@ export default () => {
 
   return (
     <Provider value={context}>
-    <div style={{background: 'White', padding: 20, margin: 20}}>
+    <div style={{padding: 20}}>
       <Route state={routerState} />
       <hr />
       <Link to={"home"}>Home</Link>
