13e4074a469937de15d8172adb1214e8cc043781
rjdestigter <johndestigter@gmail.com>
2020-02-28 10:29:26 -0700

    Updates

---
+++ precache-manifest.76075f04efc415cbeb2f778e928dc333.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "de0dc0d6d9ee62ede79035b2955945dc",
+    "url": "/xstate-sample-kit/index.html"
+  },
+  {
+    "revision": "b6a71e89f9d6b089c2e8",
+    "url": "/xstate-sample-kit/static/css/2.8e83016e.chunk.css"
+  },
+  {
+    "revision": "befa474785d7632b4a92",
+    "url": "/xstate-sample-kit/static/css/main.28f946f5.chunk.css"
+  },
+  {
+    "revision": "b6a71e89f9d6b089c2e8",
+    "url": "/xstate-sample-kit/static/js/2.add399fe.chunk.js"
+  },
+  {
+    "revision": "39f604c28f026c60b66f2aef5485aba0",
+    "url": "/xstate-sample-kit/static/js/2.add399fe.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "befa474785d7632b4a92",
+    "url": "/xstate-sample-kit/static/js/main.e5c23cfd.chunk.js"
+  },
+  {
+    "revision": "e53eabc3f7bdfb6841e5",
+    "url": "/xstate-sample-kit/static/js/runtime-main.1a8f97ad.js"
+  }
+]);
